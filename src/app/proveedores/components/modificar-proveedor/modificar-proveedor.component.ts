ed7409ef3df9f6b6324dd37ff96e7031fa858c87
Nahuel Moyano <naku.24moyano@gmail.com>
2023-08-03 10:22:56 -0300

    integrando componente de bsuqueda en tabla de modifcar proveedores

--- src/app/proveedores/components/modificar-proveedor/modificar-proveedor.component.ts
+++ src/app/proveedores/components/modificar-proveedor/modificar-proveedor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-modificar-proveedor',
@@ -8,45 +9,76 @@ import { Component, OnInit } from '@angular/core';
 export class ModificarProveedorComponent implements OnInit {
   labels: any[];
   listados: any[];
+  cities: any[];
+
+  loading: boolean = true;
+
   constructor() {
-    this.listados = [
-      {
-        num: 1,
-        nombre: 'Danilo',
-        cuit: 2033152978,
-        telefono: 323123123,
-        email: 'naku@gmail.com',
-        localidad: '',
-        provincia: 'Misiones',
-        tipo: '',
-        fantasia: 'fantasia',
-        estado: 'Habilitado',
-      },
-      {
-        num: 2,
-        nombre: 'Danilo',
-        cuit: 2033152978,
-        telefono: 323123123,
-        email: 'naku@gmail.com',
-        localidad: '',
-        provincia: 'Misiones',
-        tipo: '',
-        fantasia: 'fantasia',
-        estado: 'Habilitado',
-      },
-      {
-        num: 2,
-        nombre: 'Danilo',
-        cuit: 2033152978,
-        telefono: 323123123,
-        email: 'naku@gmail.com',
-        localidad: '',
-        provincia: 'Misiones',
-        tipo: '',
-        fantasia: 'fantasia',
-        estado: 'Deshabilitado',
-      },
-    ];
+    this.loading = true;
+
+    setTimeout(() => {
+      this.listados = [
+        {
+          num: 1,
+          nombre: 'Danilo',
+          cuit: 2033152978,
+          telefono: 323123123,
+          email: 'naku@gmail.com',
+          localidad: '',
+          provincia: 'Misiones',
+          tipo: '',
+          fantasia: 'fantasia',
+          estado: 'Habilitado',
+        },
+        {
+          num: 2,
+          nombre: 'Danilo',
+          cuit: 2033152978,
+          telefono: 323123123,
+          email: 'naku@gmail.com',
+          localidad: '',
+          provincia: 'Misiones',
+          tipo: '',
+          fantasia: 'fantasia',
+          estado: 'Habilitado',
+        },
+        {
+          num: 2,
+          nombre: 'Danilo',
+          cuit: 2033152978,
+          telefono: 323123123,
+          email: 'naku@gmail.com',
+          localidad: '',
+          provincia: 'Misiones',
+          tipo: '',
+          fantasia: 'fantasia',
+          estado: 'Deshabilitado',
+        },
+        {
+          num: 3,
+          nombre: 'Juan',
+          cuit: 2033152978,
+          telefono: 323123123,
+          email: 'naku@gmail.com',
+          localidad: '',
+          provincia: 'Misiones',
+          tipo: '',
+          fantasia: 'fantasia',
+          estado: 'Deshabilitado',
+        },
+      ];
+
+      this.cities = [
+        { name: 'Ciudad', code: '' },
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' },
+      ];
+
+      this.loading = false;
+    }, 2000);
   }
 
   ngOnInit(): void {
@@ -63,5 +95,9 @@ export class ModificarProveedorComponent implements OnInit {
         icon: 'pi pi-chevron-right',
       },
     ];
+  }
+
+  clear(table: Table) {
+    table.clear();
   }
 }
